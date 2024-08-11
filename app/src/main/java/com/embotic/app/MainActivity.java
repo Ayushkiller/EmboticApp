9a9dcec1e3382d2c9d0dc810f8367d1b0b9351b1
Ayushkiller <152204318+Ayushkiller@users.noreply.github.com>
2024-08-11 15:18:07 +0530

    cx

--- app/src/main/java/com/embotic/app/MainActivity.java
+++ app/src/main/java/com/embotic/app/MainActivity.java
@@ -15,19 +15,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWebView = findViewById(R.id.activity_main_webview);
+        mWebView = findViewById(R.id.webView); // Updated ID here
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new MyWebViewClient());
 
         // REMOTE RESOURCE
-         mWebView.loadUrl("https://dash.embotic.xyz");
-
+        mWebView.loadUrl("https://dash.embotic.xyz");
     }
 
     @Override
     public void onBackPressed() {
-        if(mWebView.canGoBack()) {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
             super.onBackPressed();
